589e27fe4f0ee18572e1514d0ab869b250703fae
MM <mmpyro@gmail.com>
2015-08-23 17:49:03 +0200

    beta version of console service

---
+++ ConsoleWatcher/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.IO;
+using System.ServiceModel;
+using FileNotifier;
+using FileWatcherService;
+
+namespace ConsoleWatcher
+{
+    public class PluginLoader
+    {
+        [ImportMany(typeof (IFileNotifier))]
+        public IEnumerable<IFileNotifier> Notifiers { get; set; }
+
+
+        public void LoadPlugins()
+        {
+            var catalog = new AggregateCatalog();
+            catalog.Catalogs.Add(new DirectoryCatalog(Path.Combine(Directory.GetCurrentDirectory(), "Plugins")));
+            var container = new CompositionContainer(catalog);
+            container.ComposeParts(this);
+        }
+    }
+
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                var pluginLoader = new PluginLoader();
+                pluginLoader.LoadPlugins();
+                var notifiers = new List<IFileNotifier>(pluginLoader.Notifiers);
+                notifiers.Add(new FakeNotifier());
+                FileObserver.CreateFunction = (dto, notifier) => new FileWatchDog(dto, notifiers.ToArray());
+                IFileNotifierManager fileNotifierManager = new FileNotifierManager(new FakeNotifier());
+                using (var service = new ServiceHost(fileNotifierManager))
+                {
+                    service.Open();
+                    fileNotifierManager.Set(new ObserveFileDto(@"D:\test"));
+                    Console.WriteLine("Service Started");
+                    Console.WriteLine("To stop service press any key ...");
+                    Console.ReadLine();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                Console.WriteLine("Service Stoped");
+                Console.ReadKey();
+            }
+        }
+
+    }
+}
---
+++ TextFileNotifierPlugin/TextFileNotifierPlugin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.IO;
+using FileNotifier;
+
+namespace TextFileNotifierPlugin
+{
+    [Export(typeof(IFileNotifier))]
+    public class TextFileNotifierPlugin : IFileNotifier
+    {
+        private readonly string _fileName;
+
+        public TextFileNotifierPlugin()
+        {
+            var dt = DateTime.Now;
+            _fileName = Path.Combine(Path.GetTempPath(), "Notifierlog.txt");
+        }
+
+        public void OnCreated(FileSystemEventArgs arg)
+        {
+            SaveIntoFile(string.Format("[{0}]: {1}, {2}", DateTime.Now, arg.ChangeType, arg.FullPath));
+        }
+
+        public void OnRename(RenamedEventArgs arg)
+        {
+            SaveIntoFile(string.Format("[{0}]: {1} from {3} to {4}, {2}", DateTime.Now, arg.ChangeType, arg.FullPath, arg.OldName, arg.Name));
+        }
+
+        private void SaveIntoFile(string value)
+        {
+            using (var sw = new StreamWriter(_fileName, true))
+            {
+                sw.WriteLine(value);
+            }
+        }
+    }
+}
